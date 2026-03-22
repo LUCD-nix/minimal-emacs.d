f0326e2506551cb3cbcdc8128bd4384fc87e1859
Lucas Dupuy <lucascordu@gmail.com>
2026-03-22 23:56:27 +0100

    restructure, outline-minor-mode in el files, c/c++ LSPs

--- post-init.el
+++ post-init.el
@@ -1,14 +1,17 @@
-;; toggles line wrap and visual line navigation
-(visual-line-mode 1)
+;;; post-init.el --- most packages go here  -*- lexical-binding: t; -*-
 
+;;; Custom file
 ;; This is already set in early-init.el
 ;; (setq custom-file "~/.config/emacs/custom.el")
 (load custom-file 'noerror 'nomessage)
 
+;;; No dir-locals.el
 (setq enable-dir-local-variables nil)
 
-;;; Enable automatic insertion and management of matching pairs of characters
-;;; (e.g., (), {}, "") globally using `electric-pair-mode'.
+
+;;; Electric pair mode
+;; Enable automatic insertion and management of matching pairs of characters
+;; (e.g., (), {}, "") globally using `electric-pair-mode'.
 (use-package elec-pair
   :ensure nil
   :commands (electric-pair-mode
@@ -16,6 +19,7 @@
              electric-pair-delete-pair)
   :hook (after-init . electric-pair-mode))
 
+;;; Misc
 ;; Allow Emacs to upgrade built-in packages, such as Org mode
 (setq package-install-upgrade-built-in t)
 
@@ -23,6 +27,9 @@
 ;; if the selection is active.
 (delete-selection-mode 1)
 
+;;; Theme various
+;; toggles line wrap and visual line navigation
+(visual-line-mode t)
 ;; Display the current line and column numbers in the mode line
 (setq line-number-mode t)
 (setq column-number-mode t)
@@ -44,16 +51,6 @@
 ;; Set the maximum level of syntax highlighting for Tree-sitter modes
 (setq treesit-font-lock-level 4)
 
-(use-package which-key
-  :ensure nil ; builtin
-  :commands which-key-mode
-  :hook (after-init . which-key-mode)
-  :custom
-  (which-key-idle-delay 1.5)
-  (which-key-idle-secondary-delay 0.25)
-  (which-key-add-column-padding 1)
-  (which-key-max-description-length 40))
-
 (unless (and (eq window-system 'mac)
              (bound-and-true-p mac-carbon-version-string))
   ;; Enables `pixel-scroll-precision-mode' on all operating systems and Emacs
@@ -65,12 +62,24 @@
   (setq pixel-scroll-precision-use-momentum nil) ; Precise/smoother scrolling
   (pixel-scroll-precision-mode 1))
 
-;; Display the time in the modeline
-(add-hook 'after-init-hook #'display-time-mode)
-
 ;; Paren match highlighting
 (add-hook 'after-init-hook #'show-paren-mode)
 
+;; Display the time in the modeline
+(add-hook 'after-init-hook #'display-time-mode)
+
+;;; which-key
+(use-package which-key
+  :ensure nil ; builtin
+  :commands which-key-mode
+  :hook (after-init . which-key-mode)
+  :custom
+  (which-key-idle-delay 1.0)
+  (which-key-idle-secondary-delay 0.25)
+  (which-key-add-column-padding 1)
+  (which-key-max-description-length 40))
+
+;;; Winner and window dividers
 ;; Track changes in the window configuration, allowing undoing actions such as
 ;; closing windows.
 (setq winner-boring-buffers '("*Completions*"
@@ -96,6 +105,7 @@
 ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Window-Dividers.html
 (add-hook 'after-init-hook #'window-divider-mode)
 
+;;; Dired stuff
 ;; Constrain vertical cursor movement to lines within the buffer
 (setq dired-movement-style 'bounded-files)
 
@@ -109,27 +119,14 @@
     (when args
       (setq dired-listing-switches args))))
 
+;;; Some minibuffer config
 ;; Enables visual indication of minibuffer recursion depth after initialization.
 (add-hook 'after-init-hook #'minibuffer-depth-indicate-mode)
 
 ;; Configure Emacs to ask for confirmation before exiting
 (setq confirm-kill-emacs 'y-or-n-p)
 
-;; Enabled backups save your changes to a file intermittently
-(setq make-backup-files t)
-(setq vc-make-backup-files t)
-(setq kept-old-versions 10)
-(setq kept-new-versions 10)
-
-;; When tooltip-mode is enabled, certain UI elements (e.g., help text,
-;; mouse-hover hints) will appear as native system tooltips (pop-up windows),
-;; rather than as echo area messages. This is useful in graphical Emacs sessions
-;; where tooltips can appear near the cursor.
-(setq tooltip-hide-delay 20)    ; Time in seconds before a tooltip disappears (default: 10)
-(setq tooltip-delay 0.4)        ; Delay before showing a tooltip after mouse hover (default: 0.7)
-(setq tooltip-short-delay 0.08) ; Delay before showing a short tooltip (Default: 0.1)
-(tooltip-mode 1)
-
+;;; Start server on emacs start
 ;; Configure the built-in Emacs server to start after initialization,
 ;; allowing the use of the emacsclient command to open files in the
 ;; current session.
@@ -138,3 +135,112 @@
   :commands server-start
   :hook
   (after-init . server-start))
+
+;;; Backups, reading modified files to buffers (autorevert)
+;; Enabled backups save your changes to a file intermittently
+(setq make-backup-files t)
+(setq vc-make-backup-files t)
+(setq kept-old-versions 10)
+(setq kept-new-versions 10)
+
+;; Auto-revert is a feature that automatically updates the
+;; contents of a buffer to reflect changes made to the underlying file
+;; on disk.
+(use-package autorevert
+  :ensure nil
+  :commands (auto-revert-mode global-auto-revert-mode)
+  :hook
+  (after-init . global-auto-revert-mode)
+  :init
+  ;; (setq auto-revert-verbose t)
+  (setq auto-revert-interval 3)
+  (setq auto-revert-remote-files nil)
+  (setq auto-revert-use-notify t)
+  (setq auto-revert-avoid-polling nil))
+
+;; Recentf is a package that maintains a list of recently
+;; accessed files, making it easier to reopen files you have worked on
+;; recently.
+(use-package recentf
+  :ensure nil
+  :commands (recentf-mode recentf-cleanup)
+  :hook
+  (after-init . recentf-mode)
+
+  :init
+  (setq recentf-auto-cleanup (if (daemonp) 300 'never))
+  (setq recentf-exclude
+        (list "\\.tar$" "\\.tbz2$" "\\.tbz$" "\\.tgz$" "\\.bz2$"
+              "\\.bz$" "\\.gz$" "\\.gzip$" "\\.xz$" "\\.zip$"
+              "\\.7z$" "\\.rar$"
+              "COMMIT_EDITMSG\\'"
+              "\\.\\(?:gz\\|gif\\|svg\\|png\\|jpe?g\\|bmp\\|xpm\\)$"
+              "-autoloads\\.el$" "autoload\\.el$"))
+
+  :config
+  ;; A cleanup depth of -90 ensures that `recentf-cleanup' runs before
+  ;; `recentf-save-list', allowing stale entries to be removed before the list
+  ;; is saved by `recentf-save-list', which is automatically added to
+  ;; `kill-emacs-hook' by `recentf-mode'.
+  (add-hook 'kill-emacs-hook #'recentf-cleanup -90))
+
+;; savehist is a feature that preserves the minibuffer history between
+;; sessions. It saves the history of inputs in the minibuffer, such as commands,
+;; search strings, and other prompts, to a file. This allows users to retain
+;; their minibuffer history across Emacs restarts.
+(use-package savehist
+  :ensure nil
+  :commands (savehist-mode savehist-save)
+  :hook
+  (after-init . savehist-mode)
+  :init
+  (setq history-length 300)
+  (setq savehist-autosave-interval 600))
+
+;; save-place-mode enables Emacs to remember the last location within a file
+;; upon reopening. This feature is particularly beneficial for resuming work at
+;; the precise point where you previously left off.
+(use-package saveplace
+  :ensure nil
+  :commands (save-place-mode save-place-local-mode)
+  :hook
+  (after-init . save-place-mode)
+  :init
+  (setq save-place-limit 400))
+
+;;; Language specific :
+;;;; Elisp
+(use-package outline
+  :ensure nil
+  :commands outline-minor-mode
+  :hook
+  ((emacs-lisp-mode . outline-minor-mode)
+   ;; Use " ▼" instead of the default ellipsis "..." for folded text to make
+   ;; folds more visually distinctive and readable.
+   (outline-minor-mode
+    .
+    (lambda()
+      (let* ((display-table (or buffer-display-table (make-display-table)))
+             (face-offset (* (face-id 'shadow) (ash 1 22)))
+             (value (vconcat (mapcar (lambda (c) (+ face-offset c)) " ▼"))))
+        (set-display-table-slot display-table 'selective-display value)
+        (setq buffer-display-table display-table))))))
+
+;;;; C/C++
+;; TODO : call to treesit-install-language-grammar a la mano at EOF?
+(add-to-list 'treesit-language-source-alist
+             '(c . ("https://github.com/tree-sitter/tree-sitter-c" "v0.24.1")))
+(add-to-list 'treesit-language-source-alist
+             '(cpp . ("https://github.com/tree-sitter/tree-sitter-cpp" "v0.23.4")))
+
+;; TODO : configure eglot too
+(add-to-list 'major-mode-remap-alist '(c-mode . c-ts-mode))
+(add-to-list 'major-mode-remap-alist '(c++-mode . c++-ts-mode))
+(add-to-list 'major-mode-remap-alist
+             '(c-or-c++-mode . c-or-c++-ts-mode))
+
+(add-to-list 'major-mode-remap-alist '(c-mode . c-ts-mode))
+(add-to-list 'major-mode-remap-alist '(c++-mode . c++-ts-mode))
+(add-to-list 'major-mode-remap-alist
+             '(c-or-c++-mode . c-or-c++-ts-mode))
+
